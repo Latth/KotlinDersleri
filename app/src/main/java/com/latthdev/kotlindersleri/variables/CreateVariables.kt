c5e5c7b7baa72216c3cd4795ae169d0977a91653
Ahmet Tunç Aras <74108753+Latth@users.noreply.github.com>
2025-04-24 19:12:38 +0300

    Initial commit

---
+++ app/src/main/java/com/latthdev/kotlindersleri/variables/CreateVariables.kt
@@ -0,0 +1,53 @@
+package com.latthdev.kotlindersleri.variables
+
+
+import java.util.Date
+
+fun main() {
+    val city : String = "Izmir";
+    val country : String = "Turkey"
+    val phone : String = "+90"
+    val postCode : Int = 35000
+    val email : String = "latherield@yandex.com";
+    val job : String = "Software Engineer"
+    val stock : Int = 100;
+    val customerName : String = "Latthdev"
+    val balance : Double = 1000.50
+    val birthday : Date = Date(2002, 5, 23);
+    val salary : Double = 1400.00;
+    val isMarried : Boolean = false;
+    val productComment : String = "This is a great product!";
+    val paymentData : Date = Date(2023, 10, 1);
+    val paymentMethod : String = "Credit Card";
+    val orderPiece : Int = 2;
+    val carBrand : String = "BMW";
+    val bookName : String = "The Great Gatsby";
+    val bookPublishDate : Date = Date(1925, 4, 10);
+    val discount : Double = 0.15;
+    val rooms : Int = 3;
+    val lat : Double = 38.4192;
+    val long : Double = 27.1287;
+    val productName : String = "Book";
+    val foodPrice : Double = 15.99;
+    val brand : String = "Nike";
+    val musicName : String = "One Shot";
+    val videoTime : String = "3:45";
+    val productStar : Double = 4.5;
+    val picName : String = "pic.jpg";
+    val fileFormat : String = "jpg";
+    val color : String = "Red";
+    val colorCode : String = "#FF5733"
+    val phoneBrand : String = "Apple";
+    val screenSize : Double = 6.1;
+    val phoneWeight : Double = 174.0;
+    val nationalDay : String = "Republic Day";
+    val holidayDate : Date = Date(1923, 10, 29);
+    val reservationDate : Date = Date(2023, 10, 1);
+    val streetName : String = "Main Street";
+    val busNumber : Int = 42;
+    val leftMinutes : Int = 30;
+    val trackingCode : String = "ABC123XYZ";
+    val couponTime : Date = Date(2023, 10, 31);
+    val couponCode : String = "DISCOUNT20";
+    val billingAddress : String = "123 Main St, Izmir, Turkey";
+}
